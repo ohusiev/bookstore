50feafa84c9fc78119f51625d4ba1cd4c793bbb6
syuqranPSYQ <syuqranshamsuddin@gmail.com>
2020-06-02 02:27:22 +0300

    structure
    i just updated product and blog page. will update content and images as soon as i figure out how to put the images in DB

--- internal/products.php
+++ internal/products.php
@@ -1,4 +1,92 @@
-<h2>Products</h2>
-<p>
-This is the  page about products.
-</p>
+<style>
+
+.main-body { padding: 0 !important; } .borders-landing { width: 100%; padding-bottom: 55px; } 
+.borders-intro { box-sizing: border-box; margin-bottom: 21px; font: 400 27px/37px Poynter,serif !important; text-align: center; padding: 0 20px; } 
+.borders-fom { box-sizing: border-box; margin-bottom: 21px; text-align: center; padding: 20px; }
+.bseller-body { box-sizing: border-box; margin-bottom: 21px; font-size: 60% ; padding: 10x; color: white; text-align: center; }
+.borders-intro span.reg { font-size: 70%; position: relative; top: -4px; } 
+.bseller { background-color: grey; color: white; padding: 10px; } 
+.main { text-align: center; }
+.container { padding: 0 15px; }
+
+</style>
+
+<body>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.html">Home</a>
+            <span class="breadcrumb-item active">Products</span>
+        </div>
+    </div>
+    <div class="borders-intro">
+        <h2>Discover your next great read here at Bibliooo<span class="reg">®</span></h2>   
+    <div class="borders-fom">
+        <h6> find out more by registering with us! </h6>
+        <br>
+        <br>
+    <div class="bseller main">
+        <h3>BESTSELLERS</h3>
+        <br>
+        <br>
+    <section class="static about-sec">
+        <div class="container">
+            <div class="recomended-sec">
+                <div class="row">
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                            <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
+                            <h6> Sams Teach Yourself SQL in 10 Minutes </h6>
+                            <h6><span class="price">$15</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=1">add to cart</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
+                            <h6> CCNA: Cisco Certified Network Associate Study Guide </h6>
+                            <h6><span class="price">$50</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=11">add to cart</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
+                            <h6> Understanding the Linux Kernel, Third Edition </h6>
+                            <h6><span class="price">$30</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=9">add to cart</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
+                            <h6> Beginning Web Programming with HTML, XHTML, and CSS </h6>
+                            <h6><span class="price">$35</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=14">add to cart</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div>
+            </div>
+        </div>
+    </section>

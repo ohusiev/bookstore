21da237b6002537a42518d58a14604269be59df7
ohusiev <SashkooXp@gmail.com>
2020-06-04 01:31:28 +0300

    myordersattempt

--- internal/myorders.php
+++ internal/myorders.php
@@ -1,12 +1,17 @@
 <h2> My orders</h2>
 <?php
-require "../internal/dbconnect.php";
+require "dbconnect.php";
 //if(!isset($_SESSION['is_admin']) || $_SESSION['is_admin']==0) {
 //	die("You are not admin");
 //}
-$stmt = $mysqli->prepare("SELECT * FROM orders WHERE Customer='$_SESSION['username']'");
-$stmt->execute();
-$res = $stmt->get_result();
-$r = $res->fetch_all(MYSQLI_ASSOC);
-print json_encode($r);
+$stmt = $mysqli->prepare("SELECT * FROM orders WHERE Customer=$_SESSION[username]");
+$result = $stmt->execute();
+print <<<END
+<table class="table table-striped">
+        <thead><tr><th>ID</th><th>Product</th><th>Date</th></tr><th>Price</th></tr></thead><tbody>
+END;
+	while ($row = $result->fetch_assoc()) {
+        print "<tr><td> $row[ID] </td> $row[ID] <td> $row[ID] </td> $row[ID] <td></td><td> $row[ID] </td></tr>";
+		}
+    print "</tbody></table>";
 ?>

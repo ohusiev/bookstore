ad239e4748332566d45ad6eba95150822b82cc36
syuqranPSYQ <syuqranshamsuddin@gmail.com>
2020-06-03 15:40:40 +0300

    pleasse check logo etc

--- internal/shopinfo.php
+++ internal/shopinfo.php
@@ -1,8 +1,55 @@
-<h1 >
-This is the page about the our shop
-</h1>
+<body>
+<div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="http://localhost/bookstore/index.php?p=start">Home</a>
+            <span class="breadcrumb-item active">Shop Info</span>
+</div>
+</div>
+<body>
 
-<p>
+
+<section class="static about-sec">
+    <div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content"> 
+      <!-- ################################################################################################ -->
+      <h1>About our shop</h1>
+      <img src="https://i.pinimg.com/564x/28/d9/30/28d9303cc59dfa8dca3a520ef437d336.jpg" alt="Biblioo">
+      <p>Biblioo Webstore was launched in September 2014 to take care of your literary needs and more at anytime, anywhere! Place your online orders with us for publications and non-book items (mostly designed and manufactured in Japan) when you can’t get enough at our physical retail bookstores.
+Biblioo Privilege Card (BPC) membership programme that was launched in April 2012 allows members to enjoy regular 10% off their off-and-online purchase with Biblioo. A true benefit for book lovers to make their purchase with us.
+Biblioo Corporate Sales Office is another important pillar of our total business and serves as the general coordinator for knowledge and information dissemination to libraries, academic institutions, organizations and corporations in Greece and the region, in support of their research and development needs.
+Always re-inventing itself, Biblio continuously strives to serve the local reading population better.</p>
+<p><b>Our Core Values</b></p>
+<ul>
+<li>Professionalism</li>
+<li>Spirit of Kaizen</li>
+<li>Service Excellence</li>
+<li>Pleasant Ambience</li>
+<li>Welfare for Employees</li>
+<li>Integrity</li>
+<li>Trust</li>
+<li>Harmony</li>
+<li>Finest Range of Titles</li>
+<li>Competitive Pricing</li>
+</ul>
+
+<p><b>Company Philosophy</b></p>
+
+<p>The Company is like a stage. Books and information products are like actors and actresses. 
+Employees are like producers and directors, and customers are like the audience. 
+Once the customers come into contact with Biblio, their theatrical experience begins. 
+To receive a standing ovation from the customers, the Company must provide a well-designed stage.
+The employee must provide clear directions to the actors and actresses and they must ensure all props are in place. 
+Actors and actresses are well cared for so that they can perform their very best before the audience.</p>
+
+<p>Updated as at September 2019.</p>
+      <br>
+      <br>
+      <br>
+</section>
+
 
 <?php
 

597cbff745f62d8760b7841495babe46892b92af
ohusiev <SashkooXp@gmail.com>
2020-06-04 00:32:30 +0300

    updateTopmenu

--- internal/topmenu.php
+++ internal/topmenu.php
@@ -1,29 +1,4 @@
-<?php
-if(!isset($_SESSION['username'])){
-		print <<<END
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?p=start">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?p=shopinfo">Shop Info</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?p=products">Products</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?p=cart">Cart</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?p=blog">Blog</a>
-      </li>
-       <li class="nav-item">
-        <a class="nav-link" href="?p=contact">Contact</a>
-      </li>        
-    </ul>
-    END;
-	}else{
-    print <<<END
+
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="index.php?p=start">Home</a>
@@ -50,6 +25,3 @@ if(!isset($_SESSION['username'])){
         <a class="nav-link" href="?p=contact">Contact</a>
       </li>        
     </ul>
-    END;
-}
-?>

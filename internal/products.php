9c7fd0a61e0d94df2348d1ca47872a345ff4ff6b
syuqranPSYQ <syuqranshamsuddin@gmail.com>
2020-06-03 13:28:59 +0300

    style and content
    updated font style and blog content

--- internal/products.php
+++ internal/products.php
@@ -1,31 +1,25 @@
 <style>
-
-.main-body { padding: 0 !important; } .borders-landing { width: 100%; padding-bottom: 55px; } 
-.borders-intro { box-sizing: border-box; margin-bottom: 21px; font: 400 27px/37px Poynter,serif !important; text-align: center; padding: 0 20px; } 
-.borders-fom { box-sizing: border-box; margin-bottom: 21px; text-align: center; padding: 20px; }
-.bseller-body { box-sizing: border-box; margin-bottom: 21px; font-size: 60% ; padding: 10x; color: white; text-align: center; }
-.borders-intro span.reg { font-size: 70%; position: relative; top: -4px; } 
-.bseller { background-color: grey; color: white; padding: 10px; } 
-.main { text-align: center; }
-.container { padding: 0 15px; }
-
+.borders-intro { box-sizing: border-box; margin-bottom: 41px; !important; padding: 0 40px; }
+.borders-intro span.reg { font-size: 70%; position: relative; top: -4px; }
 </style>
-
 <body>
     <div class="breadcrumb">
         <div class="container">
-            <a class="breadcrumb-item" href="index.html">Home</a>
+            <a class="breadcrumb-item" href="http://localhost/bookstore/index.php?p=start">Home</a>
             <span class="breadcrumb-item active">Products</span>
         </div>
     </div>
     <div class="borders-intro">
-        <h2>Discover your next great read here at Bibliooo<span class="reg">®</span></h2>   
+        <h1>Discover your next great read here at Bibliooo<span class="reg">®</span></h1>   
     <div class="borders-fom">
-        <h6> find out more by registering with us! </h6>
+        <h6><a href="http://localhost/bookstore/index.php?p=register">find out more by registering with us!</a></h6>
         <br>
         <br>
     <div class="bseller main">
-        <h3>BESTSELLERS</h3>
+        <h3>Categories</h3>
+        <div class="entry-meta">
+			<span class="entry-date">June 2020</span> 
+					</div><!-- .entry-meta -->
         <br>
         <br>
     <section class="static about-sec">
@@ -35,8 +29,78 @@
                     
                     <div class="col-lg-3 col-md-6">
                         <div class="item">
-                            <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
-                            <h6> Sams Teach Yourself SQL in 10 Minutes </h6>
+                            <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h3> <br> Databases </h3>
+                            <h6><a href="http://localhost/bookstore/index.php?p=catinfo&catid=3">click here</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h3> <br> Network </h3>
+                            <h6><a href="http://localhost/bookstore/index.php?p=catinfo&catid=4">click here</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h3> Operating System </h3>
+                            <h6><a href="http://localhost/bookstore/index.php?p=catinfo&catid=2">click here</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h3> Programming Language </h3>
+                            <h6><a href="http://localhost/bookstore/index.php?p=catinfo&catid=1">click here</a></h6>
+                            <div class="hover">
+                                <a href="product-single.html"> 
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+        <br>
+        <br>
+        <br>
+    <div class="bseller main">
+        <h3>Bestsellers</h3>
+        <br>
+        <br>
+    <section class="static about-sec">
+        <div class="container">
+            <div class="recomended-sec">
+                <div class="row">
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="item">
+                            <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h6> Sams Teach Yourself Structured Query Language (SQL) in 10 Minutes </h6>
                             <h6><span class="price">$15</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=1">add to cart</a></h6>
                             <div class="hover">
                                 <a href="product-single.html"> 
@@ -48,8 +112,8 @@
                     
                     <div class="col-lg-3 col-md-6">
                         <div class="item">
-                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
-                            <h6> CCNA: Cisco Certified Network Associate Study Guide </h6>
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h6> CCNA: Cisco Certified Network Associate Study Guide (third edition) </h6>
                             <h6><span class="price">$50</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=11">add to cart</a></h6>
                             <div class="hover">
                                 <a href="product-single.html"> 
@@ -62,8 +126,8 @@
                     
                     <div class="col-lg-3 col-md-6">
                         <div class="item">
-                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
-                            <h6> Understanding the Linux Kernel, Third Edition </h6>
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
+                            <h6> Understanding the Linux Kernel for dummy, Third Edition </h6>
                             <h6><span class="price">$30</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=9">add to cart</a></h6>
                             <div class="hover">
                                 <a href="product-single.html"> 
@@ -74,7 +138,7 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="item">
-                        <img src="https://toppng.com/uploads/preview/open-logo-icon-web-11569059120grpqjdjea8.png" alt="img" width="100" height="100">
+                        <img src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/014/white_box.png" alt="img" width="100" height="100">
                             <h6> Beginning Web Programming with HTML, XHTML, and CSS </h6>
                             <h6><span class="price">$35</span> / <a href="http://localhost/bookstore/index.php?p=productinfo&pid=14">add to cart</a></h6>
                             <div class="hover">
